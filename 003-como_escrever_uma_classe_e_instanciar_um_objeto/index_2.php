33530457415327cc4b7be0a4b7adf65b5d1808f6
miguelmilagres <miguelmdemacedo@gmail.com>
2024-04-09 17:56:56 -0300

    first commit

---
+++ 003-como_escrever_uma_classe_e_instanciar_um_objeto/index_2.php
@@ -0,0 +1,31 @@
+<?php
+
+// COMO ESCREVER UMA CLASSE E INSTANCIAR UM OBJETO
+
+/* 
+Vamos fazer uma pequena implementação
+*/
+
+class Fruto
+{
+    // propriedades
+    public $nome;
+}
+
+$laranja = new Fruto();
+
+// definir o valor da propriedade
+$laranja->nome = "Laranja";
+
+// criar um outro objeto do tipo Fruto
+$ananas = new Fruto();
+$ananas->nome = 'Ananás';
+
+
+// como vamos apresentar as propriedades de um objeto?
+echo $laranja->nome;
+echo '<br>';
+echo $ananas->nome;
+
+// cada objeto criado a partir da mesma classe, contém as suas propriedades
+// de forma completamente independente.
